391945c51d12f5d920470e35d3b3d12c2e4e9216
Mustafa Uysal <mustafauyysl@gmail.com>
2021-02-18 17:43:48 -0800

    Initial Commit

---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by Mustafa on 18.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
